fb1456bf0d3e1149077276173e16208c8a533534
Timothy Jacobson <tim@jaconet.uk>
2025-01-26 15:38:27 +0000

    Apply various changes

---
+++ gnupg/pinentry-ide.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"/nix/store/hld0l87x8ln7727v2n6amlfq0yy2xqzx-jetbrains-jdk-jcef-21.0.4-b598.4/lib/openjdk/bin/java" -cp "/nix/store/hcxndp2lcbi9ql9kj399yg8dff58s8vw-idea-community-2024.3.1/idea-community/plugins/vcs-git/lib/git4idea-rt.jar:/nix/store/hcxndp2lcbi9ql9kj399yg8dff58s8vw-idea-community-2024.3.1/idea-community/lib/externalProcess-rt.jar" git4idea.gpg.PinentryApp
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,3 +1,3 @@
 # this is sourced on all invocations of the shell (unless `-f` is set)
 # set PATH or other env vars here
-# do no use commands that produce output/assume and attached tty (use .zshrc instead)
+# do no use commands that produce output/assume an attached tty (use .zshrc instead)
